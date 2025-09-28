31a1c961bebd49f1b28f314a3f1881aba7436db5
sepehr rahimi <sepehrrahimiwork@gmail.com>
2025-09-28 13:05:00 +0330

    devide project

---
+++ src/app/api/saveToExcel/noyan/route.ts
@@ -0,0 +1,84 @@
+// pages/api/saveToExcel.ts
+import * as XLSX from "xlsx";
+import * as fs from "fs";
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+// Define the path for the Excel file (change this path as needed)
+const filePath = "src/noyan.xlsx";
+// Handle POST requests
+export async function POST(req: Request) {
+  // Parse the incoming JSON data
+  const {
+    fullName,
+    phone1,
+    companyName,
+    role,
+    projectUsage,
+    projectStage,
+    importance,
+    description,
+    // sendSMS,
+  } = await req.json();
+
+  XLSX.set_fs(fs);
+
+  try {
+    let workbook;
+    let worksheet;
+
+    if (fs.existsSync(filePath)) {
+      const fileBuffer = fs.readFileSync(filePath);
+      workbook = XLSX.read(fileBuffer, { type: "buffer" });
+      worksheet = workbook.Sheets["Sheet1"];
+    } else {
+      // Create a new workbook and worksheet
+      workbook = XLSX.utils.book_new();
+      worksheet = XLSX.utils.json_to_sheet([]);
+      XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    }
+
+    // Read existing data and append new data
+    const data = XLSX.utils.sheet_to_json(worksheet);
+    data.push({
+      "نام و نام خانوادگی": fullName,
+      "تلفن 1": phone1,
+      "نام شرکت": companyName,
+      سمت: role,
+      "کاربری پروژه": projectUsage,
+      "مرحله پروژه": projectStage,
+      اهمیت: importance,
+      توضیحات: description,
+    });
+
+    // Create a new worksheet and write data to it
+    const newWorksheet = XLSX.utils.json_to_sheet(data);
+    workbook.Sheets["Sheet1"] = newWorksheet;
+    XLSX.writeFile(workbook, filePath);
+
+    const text = `
+⭐ بیست و پنجمین نمایشگاه بین المللی ساختمان  
+🏗️ سرویس 360 ارایه دهنده راهکار های مدیریت پروژه های برق و جریان ضعیف ساختمان  
+📞 09928377982
+`;
+    const res = await axios.post(
+      "https://rest.payamak-panel.com/api/SendSMS/SendSMS",
+      {
+        username: "09121725326",
+        password: "31#R2",
+        to: phone1,
+        text,
+        from: "50002710025336",
+      }
+    );
+
+    // console.log(res);
+
+    return NextResponse.json(
+      { message: "با موفقیت انجام شد" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ src/app/service360/page.tsx
@@ -0,0 +1,218 @@
+"use client";
+
+// pages/index.tsx
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+
+type FormData = {
+  fullName: string;
+  phone1: string;
+  companyName: string;
+  role: string;
+  // projectUsage: string;
+  // projectStage: string;
+  // importance: string;
+  // description: string;
+  // sendSMS: boolean;
+};
+
+export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isLoading },
+  } = useForm<FormData>({
+    defaultValues: {
+      sendSMS: true,
+    },
+  });
+  const [message, setMessage] = useState<{ message: string; isError: boolean }>(
+    {
+      message: "",
+      isError: false,
+    }
+  );
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      const response = await axios.post("/api/saveToExcel", data);
+      if (response.status === 200) {
+        setMessage({ message: response.data.message, isError: false });
+        reset(); // Clears the form after successful submission
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log(error);
+
+      setMessage({
+        isError: true,
+        message: error.response.data.message || "Error",
+      });
+      return error;
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center bg-gray-100 h-[100vh]">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white p-6 rounded shadow-md w-full max-w-md "
+      >
+        <h1 className="text-2xl font-bold mb-4">Enter Project Data</h1>
+
+        {/* Other form fields remain the same... */}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            نام و نام خانوادگی
+          </label>
+          <input
+            type="text"
+            {...register("fullName", { required: "This field is required" })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
+          {errors.fullName && (
+            <p className="text-red-500 text-sm">{errors.fullName.message}</p>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            تلفن 1
+          </label>
+          <input
+            type="text"
+            {...register("phone1", { required: "This field is required" })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
+          {errors.phone1 && (
+            <p className="text-red-500 text-sm">{errors.phone1.message}</p>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            نام شرکت
+          </label>
+          <input
+            type="text"
+            {...register("companyName", { required: "this field is required" })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
+          {errors.companyName && (
+            <p className="text-red-500 text-sm">{errors.companyName.message}</p>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            سمت
+          </label>
+          <input
+            type="text"
+            {...register("role", { required: "this field is required" })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
+          {errors.role && (
+            <p className="text-red-500 text-sm">{errors.role.message}</p>
+          )}
+        </div>
+
+        {/* <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            آدرس
+          </label>
+          <input
+            type="text"
+            {...register("address", {})}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
+          {errors.address && (
+            <p className="text-red-500 text-sm">{errors.address.message}</p>
+          )}
+        </div> */}
+
+        {/* <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            کاربری پروژه
+          </label>
+          <div className="flex gap-4">
+            {["ویلایی", "مسکونی", "تجاری", "اداری", "سایر"].map((option) => (
+              <label key={option} className="flex items-center">
+                <input
+                  type="radio"
+                  value={option}
+                  {...register("projectUsage", {})}
+                  className="mr-2"
+                />
+                {option}
+              </label>
+            ))}
+          </div>
+          {errors.projectUsage && (
+            <p className="text-red-500 text-sm">
+              {errors.projectUsage.message}
+            </p>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            مرحله پروژه
+          </label>
+          <input
+            type="text"
+            {...register("projectStage")}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            اهمیت
+          </label>
+          <input
+            type="text"
+            {...register("importance")}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            توضیحات
+          </label>
+          <textarea
+            {...register("description")}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          />
+        </div> */}
+
+        {/* <div className="mb-4 flex items-center">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            ارسال پیامک
+          </label>
+          <input type="checkbox" {...register("sendSMS")} className="mr-2" />
+        </div> */}
+
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading" : "Submit"}
+        </button>
+        {message.message && (
+          <p
+            className={`mt-4 ${
+              message.isError ? "text-red-500" : "text-green-500"
+            }`}
+          >
+            {message.message}
+          </p>
+        )}
+      </form>
+    </div>
+  );
+}
